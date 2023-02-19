00b71581419204eb3d1a67c059872a36c8eaeb1d
rakitenkogermans <rakitenko.germans@gmail.com>
2023-02-19 21:27:06 +0200

    try catch

--- index.js
+++ index.js
@@ -10,20 +10,25 @@ const DATA = {};
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const setImageInformation = async () => {
-    const query = 'riga-old-town';
-    const orderBy = 'relevant';
-    const orientation = 'portrait';
-    const perPage = 30;
-    const img = await axios.get(`${UNSPLASH_URL}/search/photos/?client_id=${process.env.UNSPLASH_API_KEY}&query=${query}&order_by=${orderBy}&orientation=${orientation}&per_page=${perPage}`);
-    const imgCount = img.data.results.length;
-    const randomNum = randomNumber(0, imgCount);
-    const image = img.data.results[randomNum];
-    const smallImageUrl = image.urls.small;
-    const imageUsername = image.user.username;
-    const imageCreatorLink = image.user.links.html;
-    DATA.IMG_URL = smallImageUrl;
-    DATA.IMG_USERNAME = imageUsername;
-    DATA.IMG_CREATOR_LINK = imageCreatorLink;
+    try {
+        const query = 'riga-old-town';
+        const orderBy = 'relevant';
+        const orientation = 'portrait';
+        const perPage = 30;
+        const img = await axios.get(`${UNSPLASH_URL}/search/photos/?client_id=${process.env.UNSPLASH_API_KEY}&query=${query}&order_by=${orderBy}&orientation=${orientation}&per_page=${perPage}`);
+        const imgCount = img.data.results.length;
+        const randomNum = randomNumber(0, imgCount);
+        const image = img.data.results[randomNum];
+        const smallImageUrl = image.urls.small;
+        const imageUsername = image.user.username;
+        const imageCreatorLink = image.user.links.html;
+        DATA.IMG_URL = smallImageUrl;
+        DATA.IMG_USERNAME = imageUsername;
+        DATA.IMG_CREATOR_LINK = imageCreatorLink;
+    } catch(err) {
+        console.log(err);
+    }
+
 }
 
 const generateReadMe = async () => {
